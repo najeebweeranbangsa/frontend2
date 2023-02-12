d89eb1123455bafef89bbc3f1c8d1dcab32c51fc
najeeb39 <96305189+najeeb39@users.noreply.github.com>
2023-02-12 19:36:28 +0530

    added search box

--- src/components/EmployeeView.vue
+++ src/components/EmployeeView.vue
@@ -2,7 +2,45 @@
     <div >
       <h1>{{ msg }}</h1>
       <h2>Book Table</h2>
-      
+      <table class="table table-striped">
+  <thead>
+    <br>
+    <br>
+    <div class="input-group">
+    <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
+    <br><button type="button" class="btn btn-outline-primary">Search Book</button>
+    </div>
+
+    <br>
+    <tr>
+      <th scope="col">ID</th>
+      <th scope="col">Book First Name</th>
+      <th scope="col">Book Last Name</th>
+      <th scope="col">ISBN</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+
     </div>
   </template>
   
@@ -21,6 +59,9 @@
   <style scoped>
   h1, h2 {
     font-weight: normal;
+  }
+  input{
+
   }
   ul {
     list-style-type: none;
