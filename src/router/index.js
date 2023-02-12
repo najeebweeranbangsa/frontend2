26a12ee9db218d3a069a080067501338f4ceb5c1
najeeb39 <96305189+najeeb39@users.noreply.github.com>
2023-02-12 18:45:44 +0530

    setting up connecting component

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import EmployeeView from '@/components/EmployeeView'
 
 Vue.use(Router)
 
@@ -10,6 +11,12 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
-    }
+    },
+    routes: [
+      {
+        path: '/empview',
+        name: 'EmployeeView',
+        component: EmployeeView
+      },
   ]
 })
